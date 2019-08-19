32462a306e659388d51d5bc155d3fbc49e089c75
Nate <inathan.m@gmail.com>
2019-08-18 20:07:05 -0700

    f

--- utils.py
+++ utils.py
@@ -26,7 +26,7 @@ def convert(d):
 # t = histoday
 def fetch_data(length, time, sym):
     length = str(length)
-    api_uri = 'https://min-api.cryptocompare.com/data/'+time+'?fsym='+sym+'&tsym=USD&limit='+length+'&e=Kraken&api_key=fa05711eb31df6703c13a4672a8e84b65948dcb2f142a1f81b0c23b3dee57603'
+    api_uri = 'https://min-api.cryptocompare.com/data/'+time+'?fsym='+sym+'&tsym=USD&limit='+length+'&e=Bitstamp&api_key='+key
     
     d = requests.get(api_uri).json()['Data']
 
