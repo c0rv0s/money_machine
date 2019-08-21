9ce1a3ae057c9193314b2209bbd0cc14b98ca5b4
Nate <inathan.m@gmail.com>
2019-08-21 03:01:35 -0700

    g

--- tokens.py
+++
@@ -1,6 +0,0 @@
-bot_token = '832441474:AAGeWzau9CZxInYkFmislgmnrK0vj8GzNGw'
-chat_id = '-1001407136445'
-key = 'fa05711eb31df6703c13a4672a8e84b65948dcb2f142a1f81b0c23b3dee57603'
-
-api_key = 'kHl9qO7oBPldBY7amC'
-private_key = '43cZPWN7tFt8GzGrekV03E1skXqAsjRdzsul'
