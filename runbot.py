c1188d388b2f20f0c0eaf8e60385666f96e69e7e
Nate <inathan.m@gmail.com>
2019-09-10 19:58:29 -0700

    b

--- runbot.py
+++ runbot.py
@@ -6,10 +6,14 @@ data = fetch_data(1000, 'histoday', 'BTC')
 
 signal = mm_job('BTC', 'histoday', data)
 print(signal)
+
 last_price = float( data[-1]['close'] )
 
+if signal == False:
+    telegram_bot_sendtext('no change')
+
 #go long and send a message to telegram
-if signal == 'up':
+elif signal == 'up':
     o = open_order(last_price)
     if o == 0:
         telegram_bot_sendtext('XBT: bot going long')
@@ -17,7 +21,7 @@ if signal == 'up':
         telegram_bot_sendtext('XBT: up trend begin, bot failed to open long')
 
 #the other thing
-if signal == 'down':
+elif signal == 'down':
     o = close_position()
     bal = get_bal()['wallet_balance']
     
@@ -25,4 +29,5 @@ if signal == 'down':
         telegram_bot_sendtext('XBT: bot closing long, standing by for next entry, current balance is ' + str(bal))
     else:
         telegram_bot_sendtext('XBT: up trend over, bot failed to close position, current balance is ' + str(bal))
+
 
