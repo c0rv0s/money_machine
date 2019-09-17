a6719699c961cff754c52984affe7628b0f5f7f0
Nate <inathan.m@gmail.com>
2019-09-16 17:01:07 -0700

    price report

--- runbot.py
+++ runbot.py
@@ -5,15 +5,17 @@ print('runbot')
 data = fetch_data(1000, 'histoday', 'BTC')
 
 signal = mm_job('BTC', 'histoday', data)
+print('Signal: ' + str(signal))
 
 last_price = float( data[-1]['close'] )
+print('last price: '+str(last_price))
 
 if signal == False:
-    telegram_bot_sendtext('no change')
+    telegram_bot_sendtext('no change, last price: ' + str(last_price))
 
 #go long and send a message to telegram
 elif signal == 'up':
-    telegram_bot_sendtext('XBT: new trend is up')
+    telegram_bot_sendtext('XBT: new trend is up, last price: ' + str(last_price))
     o = open_order(last_price)
     if o['ret_code'] == 0:
         telegram_bot_sendtext('XBT: bot going long')
@@ -22,7 +24,7 @@ elif signal == 'up':
 
 #the other thing
 elif signal == 'down':
-    telegram_bot_sendtext('XBT: new trend is down')
+    telegram_bot_sendtext('XBT: new trend is down, last price: ' + str(last_price))
     o = close_position()
     bal = get_bal()['wallet_balance']
     
@@ -30,3 +32,5 @@ elif signal == 'down':
         telegram_bot_sendtext('XBT: bot closing long, standing by for next entry, current balance is ' + str(bal))
     else:
         telegram_bot_sendtext('XBT: up trend over, bot failed to close position, current balance is ' + str(bal)+' error report: '+str(o))
+
+
