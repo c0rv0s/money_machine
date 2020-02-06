06ce9a4911b3b29a61c2844ade0ea9df90a60585
Nate <inathan.m@gmail.com>
2020-02-05 20:35:50 -0800

    add price to message

--- chat.py
+++ chat.py
@@ -12,7 +12,6 @@ def main():
         for message in messages['result']:
             command = message['message']['text'].lower()
             chat_id = str(message['message']['chat']['id'])
-            print(message)
             if command == 'balance' or command == 'wallet':
                 data = fetch_data(1, 'histoday', 'BTC')
                 last_price = float( data[-1]['close'] )
@@ -20,20 +19,24 @@ def main():
                 bal = get_bal()
                 responseMessage = ""
                 if bal['position_value']:
-                    responseMessage = """Open position value (BTC): {}
+                    responseMessage = """
+                    Open position value (BTC): {}
                     Entry price: ${}
                     Unrealised Pnl (BTC): {}
                     Wallet Balance (BTC): {}
                     Unrealised Pnl ($$): {}
                     Wallet Balance ($$): {}
-                    """.format(bal['position_value'], bal['entry_price'], bal['unrealised_pnl'], bal['wallet_balance'], last_price*float(bal['unrealised_pnl']), last_price*float(bal['wallet_balance']))
+                    Current BTC Price: ${} 
+                    """.format(bal['position_value'], bal['entry_price'], bal['unrealised_pnl'], bal['wallet_balance'], last_price*float(bal['unrealised_pnl']), last_price*float(bal['wallet_balance']), last_price)
                 else:
-                    responseMessage = """Wallet Balance (BTC): {}
+                    responseMessage = """
+                    Wallet Balance (BTC): {}
                     Wallet Balance ($$): {}
-                    """.format(bal['wallet_balance'], last_price*float(bal['wallet_balance']))
+                    Current BTC Price: ${} 
+                    """.format(bal['wallet_balance'], last_price*float(bal['wallet_balance']), last_price)
                 telegram_bot_sendtext(responseMessage, chat_id)
         lastUpdate = messages['result'][-1]['update_id'] + 1
-        time.sleep(30)
+        time.sleep(10)
 
 if __name__ == '__main__':
     main()
